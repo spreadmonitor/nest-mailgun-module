d3c5f26ddb73abb1151a07f8442065d1056a4bc4
Attila Olah <NoNameProvided@users.noreply.github.com>
2019-07-02 15:08:03 +0200

    build: add entry point to module

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './interfaces';
+export * from './services';
+
+export * from './base-template.class';
+export * from './injection-tokens.enum';
+export * from './mailgun.module';
+export * from './module-options.class';
