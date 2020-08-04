e0dd6d8537d227b364611df7d7ea9f7fc36d56f0
Attila Olah <NoNameProvided@users.noreply.github.com>
2020-08-04 19:35:29 +0200

    build: fix Jest test config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
+// const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   preset: 'ts-jest',
@@ -13,5 +13,5 @@ module.exports = {
       tsConfig: './tsconfig.spec.json',
     },
   },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
